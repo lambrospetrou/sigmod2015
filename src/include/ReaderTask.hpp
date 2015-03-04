f610043070079f35e4b8d01f986ca714fb550c9d
Lambros Petrou <lambrospetrou@gmail.com>
2015-03-04 23:44:08 +0000

    added AsyncReaderIO to the ReaderIO

--- src/include/ReaderTask.hpp
+++ src/include/ReaderTask.hpp
@@ -8,9 +8,16 @@
 #include <cstdio>
 #include <cstdlib>
 #include <vector>
+#include <thread>
+#include <chrono>
 
 #include "LPUtils.hpp"
 #include "ReferenceTypes.hpp"
+
+
+#include "circularfifo_memory_relaxed_aquire_release.hpp"
+#include "concurrentqueue.h"
+#include "readerwriterqueue.h"
 
 namespace lp {
 
@@ -18,16 +25,20 @@ namespace lp {
         MessageHead head;
         std::vector<char> data;
     };
-    
+
+    //typedef CircularFifo<ReceivedMessage*, 5000> LPMsgQ;
+    //typedef moodycamel::ConcurrentQueue<ReceivedMessage*> LPMsgQ;
+    typedef moodycamel::ReaderWriterQueue<ReceivedMessage*> LPMsgQ;
+
     class ReaderIO {
         public:
             ReaderIO() {
                 std::ios_base::sync_with_stdio(false);
                 setvbuf ( stdin , NULL , _IOFBF , 1<<22 );
             }
+            virtual ~ReaderIO() {};
             static ReceivedMessage* readInput(FILE *instream);
             static ReceivedMessage* readInput(std::istream& instream);
-            
             virtual ReceivedMessage* nextMsg() = 0;
     };
 
@@ -59,7 +70,7 @@ namespace lp {
         return msg;
     }
 
-    
+
     ReceivedMessage* ReaderIO::readInput(std::istream& instream) { 
         // request place from the message queue - it blocks if full
         ReceivedMessage *msg = new ReceivedMessage();
@@ -83,7 +94,7 @@ namespace lp {
         if (unlikely(!instream)) { std::cerr << "read error" << std::endl; abort(); }                 
         return msg;
     }
-    
+
 
     class CReaderIO : public ReaderIO {
         private:
@@ -118,7 +129,7 @@ namespace lp {
                 mEnd = (int32_t)mInstream.tellg() + mLen;
                 //std::cerr << "len:" << mLen << std::endl;
             }
-            ~CppReaderIOTest(){}
+            ~CppReaderIOTest() {}
             virtual inline ReceivedMessage* __attribute__((always_inline)) nextMsg() override {
                 if (mLen > 0 && (int32_t)mInstream.tellg()==mEnd) {
                     //Skip result reference data
@@ -133,12 +144,63 @@ namespace lp {
             }
     };
 
+    class AsyncReaderIO : public ReaderIO {
+        private: 
+            ReaderIO *mReader;
+            LPMsgQ *mMsgQ;
+            std::thread *mThread;
+        public:
+            AsyncReaderIO(ReaderIO *reader) : mReader(reader) {
+                mMsgQ = new LPMsgQ();
+                mThread = new std::thread([this] { ReaderTask(mMsgQ, mReader); });
+            }
+            ~AsyncReaderIO() {  
+                mThread->join();
+                delete mThread;
+                delete mReader;
+                delete mMsgQ;
+            }
+            
+            // this will be called by an outsider - Single Reader ONLY
+            inline ReceivedMessage* __attribute__((always_inline)) nextMsg() override {
+                ReceivedMessage *msg; 
+                while (!mMsgQ->try_dequeue(msg)) {/*cerr<<"m ";*/ lp::lp_spin_sleep(std::chrono::microseconds(0));}
+                return msg;   
+            }
+        
+        protected:
+            // this is the Single Producer on the other side of the queue
+            void ReaderTask(LPMsgQ *msgQ, ReaderIO *msgReader) {
+#ifdef LPDEBUG
+                //auto start = LPTimer.getChrono();
+#endif
+                while (true) {
+                    //ReceivedMessage *msg = ReaderIO::readInput(stdin);
+                    ReceivedMessage *msg = msgReader->nextMsg();
+                    if (lp::unlikely(msg->head.type == MessageHead::Done)) {
+                        // exit the loop since the reader has finished its job
+                        //while (!msgQ.push(msg)) { lp_spin_sleep(); }
+                        while (!msgQ->enqueue(msg)) { lp::lp_spin_sleep(); }
+#ifdef LPDEBUG
+                        //LPTimer.readingTotal += LPTimer.getChrono(start);
+#endif
+                        return;
+                    }
+                    //while (!msgQ.push(msg)) { /*cerr << "r" << std::endl;*/ lp_spin_sleep(std::chrono::microseconds(0)); }
+                    while (!msgQ->enqueue(msg)) { /*cerr << "r" << std::endl;*/ lp::lp_spin_sleep(std::chrono::microseconds(0)); }
+                }
+                return;
+            }
+    };
 
     // Factory class that creates the proper ReaderIO class based on the arguments
     class ReaderIOFactory {
         public:
             static ReaderIO* create(FILE *mCin, bool simulateTestdriver);
             static ReaderIO* create(std::istream& mCin, bool simulateTestdriver);
+            
+            static ReaderIO* createAsync(FILE *mCin, bool simulateTestdriver);
+            static ReaderIO* createAsync(std::istream& mCin, bool simulateTestdriver);
     };
 
     ReaderIO* ReaderIOFactory::create(FILE *mCin, bool simulateTestdriver = false) {
@@ -148,6 +210,12 @@ namespace lp {
     ReaderIO* ReaderIOFactory::create(std::istream& mCin, bool simulateTestdriver = false) {
         if (simulateTestdriver) return new CppReaderIOTest(mCin);
         return new CppReaderIO(mCin);
+    }
+    ReaderIO* ReaderIOFactory::createAsync(FILE *mCin, bool simulateTestdriver = false) {
+        return new AsyncReaderIO(create(mCin, simulateTestdriver));
+    }
+    ReaderIO* ReaderIOFactory::createAsync(std::istream& mCin, bool simulateTestdriver = false) {
+        return new AsyncReaderIO(create(mCin, simulateTestdriver));
     }
 
 };
