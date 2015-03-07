86679fc5aaa6dc61db11dc85d2855bcc5257b5f5
Lambros Petrou <lambrospetrou@gmail.com>
2015-03-07 23:59:45 +0000

    replaced queue with vector in the Multipool

--- src/include/MultiTaskPool.hpp
+++ src/include/MultiTaskPool.hpp
@@ -16,6 +16,8 @@
 class MultiTaskPool {
     private:
 
+        const static uint64_t TASKS_FREE_THRESHOLD = 1<<16;
+
         //template<typename T> using LPFunc = std::function<void(uint32_t, uint32_t, T)>;
         typedef std::function<void(uint32_t, uint32_t, void*)> LPFunc;
 
@@ -26,7 +28,8 @@ class MultiTaskPool {
 
     public:
         MultiTaskPool(uint32_t tsz) : mNumOfThreads(tsz), 
-        mWaiting(0), mPoolStopped(false), mPoolRunning(false), mHelperId(tsz) {
+        mWaiting(0), mPoolStopped(false), mPoolRunning(false), mHelperId(tsz), mTasksDeqIdx(0) {
+            mTasks.reserve(1<<20);
         }
 
         void initThreads() {
@@ -38,23 +41,18 @@ class MultiTaskPool {
         inline void startAll() {
             std::unique_lock<std::mutex> lk(mMutex);
             mPoolRunning = true;
-            lk.unlock();
             mCondActive.notify_all();
+            lk.unlock();
         }
 
-        inline void waitAllAndStop() {
-            std::unique_lock<std::mutex> lk(mMutex);
-            mCondMaster.wait(lk, [this]{
-                    return (mWaiting == mNumOfThreads && mTasks.empty());
-                    });
-            mPoolRunning = false;
-            lk.unlock();
-        }
         inline void waitAll() {
             std::unique_lock<std::mutex> lk(mMutex);
             mCondMaster.wait(lk, [this]{
-                    return (mWaiting == mNumOfThreads && mTasks.empty());
+                    return (mWaiting == mNumOfThreads && isTasksEmpty());
                     });
+            if (mTasksDeqIdx >= TASKS_FREE_THRESHOLD) {
+                mTasks.resize(0); mTasksDeqIdx = 0;
+            }
             lk.unlock();
         }
 
@@ -78,14 +76,16 @@ class MultiTaskPool {
             t.func = f;
             t.args = args;
             std::lock_guard<std::mutex> lk(mMutex);
-            mTasks.push(std::move(t));
+            //mTasks.push(std::move(t));
+            mTasks.push_back(std::move(t));
             mCondActive.notify_one();
         }
         inline void addTaskUnsafe(LPFunc f, void* args) {
             Task t;
             t.func = f;
             t.args = args;
-            mTasks.push(std::move(t));
+            //mTasks.push(std::move(t));
+            mTasks.push_back(std::move(t));
         }
 
     private:
@@ -98,12 +98,17 @@ class MultiTaskPool {
         uint64_t mWaiting;
         bool mPoolStopped, mPoolRunning;
 
-        std::queue<Task> mTasks;
-
         std::atomic<uint32_t> mHelperId;
+        
+        //std::queue<Task> mTasks;
+        std::vector<Task> mTasks;
+        uint64_t mTasksDeqIdx;
+
+
+        inline bool isTasksEmpty() { /*return mTasks.empty();*/ return mTasks.size() == mTasksDeqIdx; }
 
         bool inline canProceed() {
-            return ((mPoolRunning && !mTasks.empty()) || mPoolStopped);
+            return ((mPoolRunning && !isTasksEmpty()) || mPoolStopped);
         }
 
         void worker(uint32_t tid) {
@@ -113,13 +118,14 @@ class MultiTaskPool {
                 ++mWaiting;
                 //cerr << ">" << endl;
                 // signal for synchronization!!! - all threads are waiting
-                if (mWaiting == mNumOfThreads && mTasks.empty()) mCondMaster.notify_one();
-                if (!canProceed()) mCondActive.wait(lk, [tid,this]{return canProceed();});
+                if (mWaiting == mNumOfThreads && isTasksEmpty()) mCondMaster.notify_one();
+                if (!canProceed()) mCondActive.wait(lk, [this]{return canProceed();});
                 --mWaiting;
                 if (mPoolStopped) { lk.unlock(); return; }
 
-                auto cTask = mTasks.front();
-                mTasks.pop();
+                //auto cTask = mTasks.front();
+                //mTasks.pop();
+                auto cTask = mTasks[mTasksDeqIdx++];
                 lk.unlock();
                 //cerr << "-" << endl;
 
@@ -140,16 +146,15 @@ class MultiTaskPool {
                     //std::unique_lock<std::mutex> lk(mMutex);
                     //std::cerr << ">" << std::endl;
                     // all threads are waiting and no jobs are pending
-                    if (mWaiting == mNumOfThreads ||  mTasks.empty()) {
+                    if (mWaiting == mNumOfThreads ||  isTasksEmpty()) {
                         //lk.unlock();
                         return;
                     }
                     if (mPoolStopped || !mPoolRunning) { /*lk.unlock();*/ return; }
 
-                    //auto cTask = mTasks.front();
-                    cTask = mTasks.front();
-                    mTasks.pop();
-                    //lk.unlock();
+                    //cTask = mTasks.front();
+                    //mTasks.pop();
+                    cTask = mTasks[mTasksDeqIdx++];
                 }// end of lock_guard
 
                 //std::cerr << "got job!" << std::endl;
